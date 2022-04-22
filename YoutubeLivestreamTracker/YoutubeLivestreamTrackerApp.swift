cba400c2a400f6dd3f3df02ad511f72de6b4b89d
Haotian Mo <toreimimo@gmail.com>
2022-04-21 20:13:58 -0400

    Initial Commit

---
+++ YoutubeLivestreamTracker/YoutubeLivestreamTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  YoutubeLivestreamTrackerApp.swift
+//  YoutubeLivestreamTracker
+//
+//  Created by junius7even on 2022-04-21.
+//
+
+import SwiftUI
+
+@main
+struct YoutubeLivestreamTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
